23c2fcda113778f2cbd48e33187f421cab28f6fb
Jack Matanky <JackMatanky@gmail.com>
2026-02-12 11:21:35 +0200

    feat(schema): use table definitions

--- lithos-core/src/schema/mod.rs
+++ lithos-core/src/schema/mod.rs
@@ -28,4 +28,9 @@ pub mod raw;
 /// Schema resolution service.
 pub mod resolver;
 
+use redb::TableDefinition;
+
+pub(crate) const SCHEMAS_TABLE: TableDefinition<&str, &[u8]> =
+    TableDefinition::new("schemas");
+
 // --- Public API ---
